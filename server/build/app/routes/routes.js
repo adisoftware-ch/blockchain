85647fa445b1bcc53674e2d2fb83a8902ffcbb56
Adrian Tschannen <adi@adisoftware.ch>
2019-11-03 12:36:18 +0100

    removed package server

--- server/build/app/routes/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-exports.router = express.Router();
-exports.router.get('/ping', function (_REQ, res) {
-    res.json({ alive: 'yes' }).status(200);
-});
--- server/src/app/routes/routes.ts
+++
@@ -1,7 +0,0 @@
-import express = require("express");
-
-export const router = express.Router();
-
-router.get('/ping', (_REQ, res) => {
-    res.json({alive: 'yes'}).status(200);
-});
